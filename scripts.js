1953295b6abf8b628861e91ed1c7710016eff4dd
Nacho <nachoherrero93@hotmail.com>
2020-08-28 23:07:47 -0300

    Add javascript

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+const people = [{
+   nam: 'Tanya Sinclair',
+   profesion: 'UX Engineer',
+   description: '"I\'ve been interested in coding for a while but never taken the jump, until now. I couldn\'t recommend this course enough. I\'m now in the job of my dreams and so excited about the future."',
+   img: '../images/image-tanya.jpg'
+}, {
+   nam: 'Jon Tarkpor',
+   profesion: 'Junior Front-end Developer',
+   description: '"If you want to lay the best foundation possible I\'d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."',
+   img: '../images/image-john.jpg'
+}]
+
+let i = 0;
+
+next.addEventListener('click', () => {
+   i += 1;
+   i = i % people.length;
+   profesion.textContent = people[i].profesion;
+   nam.textContent = people[i].nam;
+   description.textContent = people[i].description;
+   img.src = people[i].img;
+});
+
+previous.addEventListener('click', () => {
+   if (i === 0) {
+      i = people.length;
+   }
+   i -= 1;
+   profesion.textContent = people[i].profesion;
+   nam.textContent = people[i].nam;
+   description.textContent = people[i].description;
+   img.src = people[i].img;
+})
+
